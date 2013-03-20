29944ba24a188c9b60562ec76ba216230bbd9401
fermi <perpect7@dags.kr>
2013-03-20 22:13:34 +0900

    1. busybox mount shell. 	modified:   mdev-mount.sh

--- meta-openpli/recipes-core/busybox/busybox-1.19.4/mdev-mount.sh
+++ meta-openpli/recipes-core/busybox/busybox-1.19.4/mdev-mount.sh
@@ -2,7 +2,7 @@
 
 notify() {
 	# we don't really depend on the hotplug_e2_helper, but when it exists, call it
-	if [ -x /usr/bin/hotplug_e2_helper ] ; then
+	if [ -x /usr/bin/hotplug_e2_helper ]; then
 		/usr/bin/hotplug_e2_helper $ACTION /block/$MDEV $PHYSDEVPATH
 	fi
 }
@@ -19,18 +19,6 @@ case "$ACTION" in
 		# check for "please don't mount it" file
 		if [ -f "/dev/nomount.${DEVBASE}" ] ; then
 			# blocked
-			exit 0
-		fi
-		# Run the result of blkid as a shell command
-		eval `blkid /dev/${MDEV} | grep ${MDEV} | cut -d ':' -f 2`
-		if [ -z "$TYPE" ] ; then
-			notify
-			exit 0
-		fi
-		if [ $TYPE == swap ] ; then
-			if ! grep -q "^/dev/${MDEV} " /proc/swaps ; then
-				swapon /dev/${MDEV}
-			fi
 			exit 0
 		fi
 		# check for full-disk partition
@@ -50,58 +38,64 @@ case "$ACTION" in
 			fi
 		fi
 		# first allow fstab to determine the mountpoint
-		if ! mount /dev/$MDEV > /dev/null 2>&1 ; then
+		if ! mount /dev/$MDEV > /dev/null 2>&1
+		then
 			# no fstab entry, use automatic mountpoint
 			REMOVABLE=`cat /sys/block/$DEVBASE/removable`
-			readlink -fn /sys/block/$DEVBASE/device | grep -qs 'pci\|ahci'
+			readlink -fn /sys/block/$DEVBASE/device | grep -qs 'pci'
 			EXTERNAL=$?
-			if [ "${REMOVABLE}" -eq "0" -a $EXTERNAL -eq 0 ] ; then
+			if [ "${REMOVABLE}" -eq "0" -a $EXTERNAL -eq 0 ]; then
 				# mount the first non-removable internal device on /media/hdd
 				DEVICETYPE="hdd"
 			else
-				if [ -z "${LABEL}" ] ; then
-					MODEL=`cat /sys/block/$DEVBASE/device/model`
-					if [ "$MODEL" == "USB CF Reader   " ]; then
-						DEVICETYPE="cf"
-					elif [ "$MODEL" == "Compact Flash   " ]; then
-						DEVICETYPE="cf"
-					elif [ "$MODEL" == "USB SD Reader   " ]; then
-						DEVICETYPE="mmc1"
-					elif [ "$MODEL" == "USB SD  Reader  " ]; then
-						DEVICETYPE="mmc1"
-					elif [ "$MODEL" == "SD/MMC          " ]; then
-						DEVICETYPE="mmc1"
-					elif [ "$MODEL" == "USB MS Reader   " ]; then
-						DEVICETYPE="mmc1"
-					elif [ "$MODEL" == "SM/xD-Picture   " ]; then
-						DEVICETYPE="mmc1"
-					elif [ "$MODEL" == "USB SM Reader   " ]; then
-						DEVICETYPE="mmc1"
-					elif [ "$MODEL" == "MS/MS-Pro       " ]; then
-						DEVICETYPE="mmc1"
-					else
-						DEVICETYPE="usb"
-					fi
+				MODEL=`cat /sys/block/$DEVBASE/device/model`
+				if [ "$MODEL" == "USB CF Reader   " ]; then
+					DEVICETYPE="cf"
+				elif [ "$MODEL" == "Compact Flash   " ]; then
+					DEVICETYPE="cf"
+				elif [ "$MODEL" == "USB SD Reader   " ]; then
+					DEVICETYPE="mmc1"
+				elif [ "$MODEL" == "USB SD  Reader  " ]; then
+					DEVICETYPE="mmc1"
+				elif [ "$MODEL" == "SD/MMC          " ]; then
+					DEVICETYPE="mmc1"
+				elif [ "$MODEL" == "USB MS Reader   " ]; then
+					DEVICETYPE="mmc1"
+				elif [ "$MODEL" == "SM/xD-Picture   " ]; then
+					DEVICETYPE="mmc1"
+				elif [ "$MODEL" == "USB SM Reader   " ]; then
+					DEVICETYPE="mmc1"
+				elif [ "$MODEL" == "MS/MS-Pro       " ]; then
+					DEVICETYPE="mmc1"
 				else
-					DEVICETYPE="${LABEL}"
+					DEVICETYPE="usb"
 				fi
 			fi
 			# Use mkdir as 'atomic' action, failure means someone beat us to the punch
 			MOUNTPOINT="/media/$DEVICETYPE"
-			if ! mkdir "${MOUNTPOINT}" ; then
-				MOUNTPOINT="/media/$MDEV"
-				mkdir -p "${MOUNTPOINT}"
+
+			# Remove mountpoint not being used
+			if [ -z "`grep $MOUNTPOINT /proc/mounts`" ];
+			then 
+				rm -rf $MOUNTPOINT
 			fi
-			mount -t auto /dev/$MDEV "${MOUNTPOINT}"
+
+			if ! mkdir $MOUNTPOINT
+			then
+				MOUNTPOINT="/media/$MDEV"
+				mkdir -p $MOUNTPOINT
+			fi
+			mount -t auto /dev/$MDEV $MOUNTPOINT
 		fi
 		;;
 	remove)
-		MOUNTPOINT=`grep "^/dev/$MDEV\s" /proc/mounts | cut -d' ' -f 2 | sed 's/\\\\040/ /g'`
-		if [ -z "${MOUNTPOINT}" ] ; then
+		MOUNTPOINT=`grep "^/dev/$MDEV\s" /proc/mounts | cut -d' ' -f 2`
+		if [ -z "$MOUNTPOINT" ]
+		then
 			MOUNTPOINT="/media/$MDEV"
 		fi
-		umount "${MOUNTPOINT}" || umount "/dev/${MDEV}"
-		rmdir "${MOUNTPOINT}"
+		umount $MOUNTPOINT || umount /dev/$MDEV
+		rmdir $MOUNTPOINT
 		;;
 	*)
 		# Unexpected keyword
